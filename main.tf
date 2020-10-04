e1615c53d1ca288175e8a2577d12421f352e1053
Pavel Akhtyamov <akhtyamovpavel@gmail.com>
2020-10-04 17:08:33 +0300

    Adding server routing

--- main.tf
+++ main.tf
@@ -5,7 +5,26 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-06e54d05255faf8f6"
     instance_type = "t2.micro"
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello world" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
     tags = {
         Name = "terraform-example"
+    }
+    
+    vpc_security_group_ids = [aws_security_group.instance.id]
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+    
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
