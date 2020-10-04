97615be2dceee7a9ce164bb7d4d5c1fc043def8d
Pavel Akhtyamov <akhtyamovpavel@gmail.com>
2020-10-04 17:41:21 +0300

    Creating autoscale group

--- main.tf
+++ main.tf
@@ -2,20 +2,45 @@ provider "aws" {
     region = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-06e54d05255faf8f6"
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-06e54d05255faf8f6"
     instance_type = "t2.micro"
-
+    
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello world" > index.html
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
-    tags = {
-        Name = "terraform-example"
-    }
     
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+
+    vpc_zone_identifier = data.aws_subnet_ids.default.ids
+    min_size = 2
+    max_size = 10
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+# Data - read only form source
+# aws - provider, vpc - virtual private cloud
+data "aws_vpc" "default" {
+    default = true
+}
+
+# Get subnet ids for launch_configuration
+data "aws_subnet_ids" "default" {
+    vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group" "instance" {
@@ -34,10 +59,4 @@ variable "server_port" {
     type = number
     default = 8080
 }
-
-output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "Public ip of server"
-}
-
 
